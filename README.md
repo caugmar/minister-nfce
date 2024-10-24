a1f33442fa97030bb493d99ae06821f43da121cd
Guto Marcicano <caugmar@gmail.com>
2024-10-24 14:12:48 -0300

    Começo da interface gráfica.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 minister-nfce - um aplicativo para emissão de NFC-e.
 
 Interface gráfica baseada em Tkinter moderno.
+com tema https://github.com/rdbende/Sun-Valley-ttk-theme
 Banco de dados sqlite3.
 Comunicação com a fazenda através da biblioteca PyNFe.
 
---
+++ minister_nfce/main.py
@@ -0,0 +1,50 @@
+from tkinter import *
+from tkinter import ttk
+import sv_ttk
+
+def calculate(*args):
+    try:
+        value = float(feet.get())
+        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
+    except ValueError:
+        pass
+
+root = Tk()
+root.title("Minister NFC-e")
+root.option_add('*tearOff', FALSE)
+menubar = Menu(root)
+menu_file = Menu(menubar)
+menu_edit = Menu(menubar)
+menubar.add_cascade(menu=menu_file, label='Cadastros')
+menu_file.add_command(label='Produtos e Serviços')
+menu_file.add_command(label='Clientes [Opcional]')
+menubar.add_cascade(menu=menu_edit, label='Emissão')
+root['menu'] = menubar
+
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+feet = StringVar()
+meters = StringVar()
+
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+
+ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+for child in mainframe.winfo_children(): 
+    child.grid_configure(padx=5, pady=5)
+
+feet_entry.focus()
+root.bind("<Return>", calculate)
+
+sv_ttk.use_light_theme()
+root.mainloop()
+
