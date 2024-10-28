35c108fb878f3f01a1e227787d09847a9276f7c7
Guto Marcicano <caugmar@gmail.com>
2024-10-28 11:09:35 -0300

    Primeiros sinais conectados.

--- minister_nfce/main.py
+++ minister_nfce/main.py
@@ -4,9 +4,13 @@ from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import QApplication
 from PySide6.QtCore import QFile, QIODevice
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
+def obter_app():
+    return QApplication(sys.argv)
 
+def saindo():
+    print("Saindo.")
+
+def obter_janela():
     ui_file_name = os.path.join(os.path.dirname(__file__), "main.ui")
     ui_file = QFile(ui_file_name)
     if not ui_file.open(QIODevice.ReadOnly):
@@ -18,6 +22,13 @@ if __name__ == "__main__":
     if not window:
         print(loader.errorString())
         sys.exit(-1)
-    window.show()
+    window.actionSair.triggered.connect(saindo)
+    window.buttonMensagem.clicked.connect(saindo)
+    return window
 
+if __name__ == "__main__":
+    app = obter_app()
+    window = obter_janela()
+    window.show()
     sys.exit(app.exec())
+
