1da2d9471f0eea68b307f95716bed95ac7b9e5ea
Guto Marcicano <caugmar@gmail.com>
2024-10-28 17:50:27 -0300

    Convertido para classe.

--- minister_nfce/main.py
+++ minister_nfce/main.py
@@ -4,31 +4,29 @@ from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import QApplication
 from PySide6.QtCore import QFile, QIODevice
 
-def obter_app():
-    return QApplication(sys.argv)
+class Aplicativo(QApplication):
 
-def saindo():
-    print("Saindo.")
+    def __init__(self, arg):
+        super().__init__(arg)
+        ui_file_name = os.path.join(os.path.dirname(__file__), "main.ui")
+        ui_file = QFile(ui_file_name)
+        if not ui_file.open(QIODevice.ReadOnly):
+            print(f"Cannot open {ui_file_name}: {ui_file.errorString()}")
+            sys.exit(-1)
+        loader = QUiLoader()
+        self.window = loader.load(ui_file)
+        ui_file.close()
+        if not self.window:
+            print(loader.errorString())
+            sys.exit(-1)
+        self.window.actionSair.triggered.connect(self.saindo)
+        self.window.buttonMensagem.clicked.connect(self.saindo)
+        self.window.show()
 
-def obter_janela():
-    ui_file_name = os.path.join(os.path.dirname(__file__), "main.ui")
-    ui_file = QFile(ui_file_name)
-    if not ui_file.open(QIODevice.ReadOnly):
-        print(f"Cannot open {ui_file_name}: {ui_file.errorString()}")
-        sys.exit(-1)
-    loader = QUiLoader()
-    window = loader.load(ui_file)
-    ui_file.close()
-    if not window:
-        print(loader.errorString())
-        sys.exit(-1)
-    window.actionSair.triggered.connect(saindo)
-    window.buttonMensagem.clicked.connect(saindo)
-    return window
+    def saindo(self):
+        sys.exit(self.exec())
 
 if __name__ == "__main__":
-    app = obter_app()
-    window = obter_janela()
-    window.show()
-    sys.exit(app.exec())
+    app = Aplicativo(sys.argv)
+    app.exec()
 
