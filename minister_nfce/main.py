aa0bff4b52e55ecfe34014681fceaaec46c73727
Guto Marcicano <caugmar@gmail.com>
2024-10-28 09:12:55 -0300

    Mudando para usar Designer e arquivos ui.

--- minister_nfce/main.py
+++ minister_nfce/main.py
@@ -1,66 +1,23 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QMenuBar, QMenu, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton
-from PySide6.QtCore import Qt, Slot
+# File: main.py
 import sys
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Minister NFC-e")
-
-        # Menu Bar
-        menu_bar = QMenuBar(self)
-        self.setMenuBar(menu_bar)
-
-        # Menus
-        menu_file = QMenu("Cadastros", self)
-        menu_file.addAction("Produtos e Serviços")
-        menu_file.addAction("Clientes [Opcional]")
-        
-        menu_edit = QMenu("Emissão", self)
-
-        # Adding Menus to the MenuBar
-        menu_bar.addMenu(menu_file)
-        menu_bar.addMenu(menu_edit)
-
-        # Main widget
-        central_widget = QWidget(self)
-        self.setCentralWidget(central_widget)
-
-        # Layout
-        layout = QVBoxLayout(central_widget)
-
-        # Input and output fields
-        self.feet_entry = QLineEdit()
-        self.feet_entry.setPlaceholderText("Enter feet")
-        self.meters_label = QLabel("")
-
-        # Calculate button
-        calculate_button = QPushButton("Calculate")
-        calculate_button.clicked.connect(self.calculate)
-
-        # Labels
-        layout.addWidget(QLabel("feet"))
-        layout.addWidget(self.feet_entry)
-        layout.addWidget(QLabel("is equivalent to"))
-        layout.addWidget(self.meters_label)
-        layout.addWidget(QLabel("meters"))
-        layout.addWidget(calculate_button)
-
-        # Add padding and alignment
-        layout.setContentsMargins(10, 10, 10, 10)
-        layout.setAlignment(Qt.AlignTop)
-
-    @Slot()
-    def calculate(self):
-        try:
-            value = float(self.feet_entry.text())
-            meters = round(0.3048 * value, 4)
-            self.meters_label.setText(str(meters))
-        except ValueError:
-            self.meters_label.setText("")
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QFile, QIODevice
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MainWindow()
+
+    ui_file_name = "main.ui"
+    ui_file = QFile(ui_file_name)
+    if not ui_file.open(QIODevice.ReadOnly):
+        print(f"Cannot open {ui_file_name}: {ui_file.errorString()}")
+        sys.exit(-1)
+    loader = QUiLoader()
+    window = loader.load(ui_file)
+    ui_file.close()
+    if not window:
+        print(loader.errorString())
+        sys.exit(-1)
     window.show()
+
     sys.exit(app.exec())
